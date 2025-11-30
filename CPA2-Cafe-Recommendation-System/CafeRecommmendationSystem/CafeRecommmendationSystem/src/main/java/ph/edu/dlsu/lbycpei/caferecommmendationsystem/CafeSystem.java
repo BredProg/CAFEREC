b232e33db302bbb5a2ade87dfcc9fc762bbce8df
JuanSoco <juan_soco@dlsu.edu.ph>
2025-11-30 19:34:53 +0800

    CafeSystem.java

--- CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/CafeSystem.java
+++ CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/CafeSystem.java
@@ -1,57 +1,70 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 package ph.edu.dlsu.lbycpei.caferecommmendationsystem;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class CafeSystem {
-
     private Menu menu = new Menu();
     private RecommendationGraph graph = new RecommendationGraph();
     private Similarity similarity = new Similarity();
-    private Scanner sc = new Scanner(System.in);
+    private Scanner sc;
 
     public CafeSystem() {
-        loadSampleMenu();
+        this.sc = new Scanner(System.in);
+        this.loadSampleMenu();
     }
 
     private void loadSampleMenu() {
-        menu.addItem(new MenuItem("Matcha Latte", 150));
-        menu.addItem(new MenuItem("Espresso", 120));
-        menu.addItem(new MenuItem("Dubai Chocolate Brownie", 95));
-        menu.addItem(new MenuItem("Iced Tea", 100));
-        menu.addItem(new MenuItem("Lemonade", 80));
+        this.menu.addItem(new MenuItem("Matcha Latte", (double)150.0F));
+        this.menu.addItem(new MenuItem("Espresso", (double)120.0F));
+        this.menu.addItem(new MenuItem("Dubai Chocolate Brownie", (double)95.0F));
+        this.menu.addItem(new MenuItem("Iced Tea", (double)100.0F));
+        this.menu.addItem(new MenuItem("Lemonade", (double)80.0F));
     }
 
     public void run() {
-        while (true) {
+        while(true) {
             System.out.println("\n1. View Menu");
             System.out.println("2. Place Order");
             System.out.println("3. Exit");
             System.out.print("Choose: ");
-
-            int choice = sc.nextInt();
-            sc.nextLine();
-
+            int choice = this.sc.nextInt();
+            this.sc.nextLine();
             switch (choice) {
-                case 1 -> menu.displayMenu();
-                case 2 -> placeOrder();
-                case 3 -> { System.out.println("Goodbye!"); return; }
+                case 1:
+                    this.menu.displayMenu();
+                    break;
+                case 2:
+                    this.placeOrder();
+                    break;
+                case 3:
+                    System.out.println("Goodbye!");
+                    return;
             }
         }
     }
 
     private void placeOrder() {
         Order order = new Order();
-        menu.displayMenu();
+        this.menu.displayMenu();
 
-        while (true) {
+        while(true) {
             System.out.print("Enter item name (or 'done'): ");
-            String input = sc.nextLine();
+            String input = this.sc.nextLine();
+            if (input.equalsIgnoreCase("done")) {
+                this.graph.updateGraph(order.getItems());
+                this.addRecommendedItems(order);
+                Receipt.printReceipt(order);
+                return;
+            }
 
-            if (input.equalsIgnoreCase("done")) break;
-
-            MenuItem item = menu.getItemByName(input);
+            MenuItem item = this.menu.getItemByName(input);
             if (item != null) {
                 order.addItem(item);
                 System.out.println("Added: " + item.getName());
@@ -59,36 +72,22 @@ public class CafeSystem {
                 System.out.println("Item not found.");
             }
         }
-
-        graph.updateGraph(order.getItems());
-
-        addRecommendedItems(order);
-
-        Receipt.printReceipt(order);
     }
 
     private void addRecommendedItems(Order order) {
-
-        while (true) {
+        while(true) {
             System.out.println("\n--- Recommended Items ---");
+            HashSet<String> recommendations = new HashSet();
 
-            HashSet<String> recommendations = new HashSet<>();
-
-            for (MenuItem item : order.getItems()) {
+            for(MenuItem item : order.getItems()) {
                 String itemName = item.getName();
-
-                String paired = graph.getTopRecommendation(itemName);
-                if (paired != null
-                        && menu.getItemByName(paired) != null
-                        && !order.containsItem(paired)) {
+                String paired = this.graph.getTopRecommendation(itemName);
+                if (paired != null && this.menu.getItemByName(paired) != null && !order.containsItem(paired)) {
                     recommendations.add(paired);
                 }
 
-                var list = similarity.getSimilarItems(itemName);
-                for (String sim : list) {
-
-                    if (menu.getItemByName(sim) != null
-                            && !order.containsItem(sim)) {
+                for(String sim : this.similarity.getSimilarItems(itemName)) {
+                    if (this.menu.getItemByName(sim) != null && !order.containsItem(sim)) {
                         recommendations.add(sim);
                     }
                 }
@@ -100,24 +99,24 @@ public class CafeSystem {
             }
 
             int index = 1;
-            ArrayList<String> recList = new ArrayList<>(recommendations);
-            for (String r : recList) {
+            ArrayList<String> recList = new ArrayList(recommendations);
+
+            for(String r : recList) {
                 System.out.println(index + ". " + r);
-                index++;
+                ++index;
             }
 
             System.out.print("Would you like to add any recommended item? (name or 'done'): ");
-            String input = sc.nextLine();
+            String input = this.sc.nextLine();
+            if (input.equalsIgnoreCase("done")) {
+                return;
+            }
 
-            if (input.equalsIgnoreCase("done")) break;
-
-            MenuItem recItem = menu.getItemByName(input);
-
+            MenuItem recItem = this.menu.getItemByName(input);
             if (recItem != null && recList.contains(recItem.getName())) {
                 order.addItem(recItem);
                 System.out.println("Added: " + recItem.getName());
-
-                graph.updateGraph(order.getItems());
+                this.graph.updateGraph(order.getItems());
             } else {
                 System.out.println("Invalid recommendation.");
             }
