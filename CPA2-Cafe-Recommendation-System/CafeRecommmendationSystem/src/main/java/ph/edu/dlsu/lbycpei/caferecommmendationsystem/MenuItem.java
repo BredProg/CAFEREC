fad3d30b1fe9719661937e6f45d566f3c3c1318b
JuanSoco <juan_soco@dlsu.edu.ph>
2025-11-30 19:46:30 +0800

    MenuItem.java

--- CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/MenuItem.java
+++ CPA2-Cafe-Recommendation-System/CafeRecommmendationSystem/src/main/java/ph/edu/dlsu/lbycpei/caferecommmendationsystem/MenuItem.java
@@ -5,13 +5,15 @@ public class MenuItem {
     private double price;
     private String category;
 
-    public MenuItem(String name, double price) {
+    public MenuItem(String name, double price, String category) {
         this.name = name;
         this.price = price;
+        this.category = category;
     }
 
     public String getName() { return name; }
     public double getPrice() { return price; }
+    public String getCategory() { return category; }
 
     @Override
     public String toString() {
